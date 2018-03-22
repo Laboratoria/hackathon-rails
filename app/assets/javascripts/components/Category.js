a99aa6d7f7acae9c6450c9f7e475ac3ad76d3022
carolina.silvag <carolinasguzman@gmail.com>
2018-03-22 19:17:18 -0300

    ahora si

---
+++ app/assets/javascripts/components/Category.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Category extends React.Component {
+  render () {
+    return (
+    
+      	<div>HOLAS!!!!</div>
+      
+    );
+  }
+}
+
+export default Category
